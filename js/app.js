88c2465cb56ee25d72ef6dca97011ce18749adb7
Dimple-Mun <wenjchn@163.com>
2019-08-13 03:22:20 +0800

    feature: 设置重启功能

--- js/app.js
+++ js/app.js
@@ -1,14 +1,38 @@
 /*
  * 创建一个包含所有卡片的数组
  */
-
+const arrDefaultValue = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb','fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb'];
 
 /*
- * 显示页面上的卡片
+ * 为重启按钮设置重启功能
  *   - 使用下面提供的 "shuffle" 方法对数组中的卡片进行洗牌
  *   - 循环遍历每张卡片，创建其 HTML
  *   - 将每张卡的 HTML 添加到页面
  */
+function restart(){
+    let targetElements
+    // 洗牌
+    const arrCardValue = shuffle(arrDefaultValue);
+    // 初始化卡片状态，包括将卡片翻转(open)，隐藏卡片值(show)，以及删除匹配状态(match)
+    targetElements = document.querySelectorAll('.deck .card');
+    for(let i = 0; i < targetElements.length; i++){
+      targetElements[i].classList.remove('open','show','match');
+    }
+    // 将随机生成的新牌面值赋给每张卡片
+    targetElements = document.querySelectorAll('.deck .fa');
+    for(let i = 0; i < targetElements.length; i++){
+      targetElements[i].classList.add(arrCardValue[i]);
+    }
+    // 重置招式数量
+    targetElements = document.getElementsByClassName('moves');
+    targetElements[0].textContent = '0';
+    // 重置星级评分
+    targetElements = document.querySelectorAll('.stars .fa');
+    for(let i = 0; i < targetElements.length; i++){
+      targetElements[i].classList.add('fa-star');
+    }
+    //TODO: 添加计时器
+  }
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle(array) {
